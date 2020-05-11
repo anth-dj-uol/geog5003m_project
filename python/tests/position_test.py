3be8b957d057bb8c35bc06bc98ae1ce85ad3fa1a
Anthony Jarrett <AnthonyJarrett@nanometrics.ca>
2020-05-11 19:15:09 -0400

    add position unit tests

---
+++ python/tests/position_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Position unit tests
+
+@author: Anthony Jarrett
+"""
+
+import unittest
+
+from python.src.abm import agentframework
+
+class PositionTestCase(unittest.TestCase):
+
+    def test_init(self):
+
+        # Initialize position parameters
+        x_pos = 1
+        y_pos = 2
+
+        # Create position
+        position = agentframework.Position(x_pos, y_pos)
+
+        # Verfiy position coordinates
+        self.assertIsNotNone(position)
+        self.assertEqual(position.x, x_pos)
+        self.assertEqual(position.y, y_pos)
+
+
+    def test_create_from(self):
+
+        # Initialize position parameters
+        x_pos = 3
+        y_pos = 4
+
+        # Create position
+        position_a = agentframework.Position(x_pos, y_pos)
+        position_b = agentframework.Position.create_from(position_a)
+
+        # Verfiy position coordinates
+        self.assertIsNotNone(position_a)
+        self.assertEqual(position_a.x, x_pos)
+        self.assertEqual(position_a.y, y_pos)
+        self.assertEqual(position_a.x, position_b.x)
+        self.assertEqual(position_a.y, position_b.y)
