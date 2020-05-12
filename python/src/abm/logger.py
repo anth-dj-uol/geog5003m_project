44d164c85126e365bca0539ecf553d7682807418
Anthony Jarrett <AnthonyJarrett@nanometrics.ca>
2020-05-11 21:53:46 -0400

    add function documentation to the logger module

--- python/src/abm/logger.py
+++ python/src/abm/logger.py
@@ -13,7 +13,25 @@ from datetime import datetime
 
 _enabled = False    # Track the logging enabled flag
 
+
 def log(message, *args):
+    """
+    Log a message.
+    
+    If logging is enabled, the message will be printed to standard output.
+
+    Parameters
+    ----------
+    message : str
+        Log message.
+    *args : any
+        Optional string format parameters.
+
+    Returns
+    -------
+    None.
+
+    """
 
     # Check if logging is enabled
     if _enabled:
@@ -21,8 +39,21 @@ def log(message, *args):
         # Print the log message with any provided format arguments
         print("{} - {}".format(datetime.now().isoformat(), message.format(*args)))
 
-def configure(enabled=False):
 
+def configure(enabled=False):
+    """
+    Configure the logger utility.
+
+    Parameters
+    ----------
+    enabled : bool, optional
+        True if logging should be enabled. The default is False.
+
+    Returns
+    -------
+    None.
+
+    """
     # Set the logging enabled flag
     global _enabled
     _enabled = enabled
